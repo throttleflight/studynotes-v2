3f20fae7a584dcef304355d748d7983769816773
Throttle Flight <airplanenews4@gmail.com>
2025-08-07 15:52:38 -0400

    Update notes.ts

--- lib/notes.ts
+++ lib/notes.ts
@@ -1951,7 +1951,7 @@ abla f(x,y)||$$
 
             Newton's laws are only valid in inertial reference frames. The reference frame isn't accelerating or rotating for Newton's first law to be true.
 
-            <strong>Concept:<strong><strong>Newton's Second Law of Motion:</strong>$$\\sum{\\vec{F}} = m\\vec{a}$$$$Newton = \\frac{kg \\cdot m}{s^2}$$The unit of force is the Newton, mass is measured in kilograms ($kg$), and acceleration is measured in meters per second squared ($\\frac{m}{s^2}$).
+            <strong>Concept:<strong><strong>Newton's Second Law of Motion:</strong>$$\\sum{\\vec{F}} = m\\vec{a}$$$$Newton = \\frac{kg \\cdot m}{s^2}$$The unit of force is the Newton, mass is measured in kilograms ($kg$), and acceleration is measured in meters per second squared ($\\frac{m}{s^2}$)test.
 
             In Newton's Second Law of Motion, $\\sum \\vec{F}$ represents the net force. The Greek letter sigma ($\\sum$) represents the sum of. Sometimes, $\\sum \\vec{F}$ is written as $\\sum \\vec{F_{net}}$ to represent net force. The arrow above the $F$ represents that it is a vector, sometimes it isn't there, but remember that it's there. 
 
