65eb4a468c4a2eb72cac5bed30ab306cbdbe7f40
Throttle Flight <airplanenews4@gmail.com>
2025-08-07 16:13:24 -0400

    Update notes.ts

--- lib/notes.ts
+++ lib/notes.ts
@@ -1951,7 +1951,7 @@ abla f(x,y)||$$
 
             Newton's laws are only valid in inertial reference frames. The reference frame isn't accelerating or rotating for Newton's first law to be true.
 
-            <strong>Definition:<strong>Newton's Second Law of Motion:$$\\sum{\\vec{F}} = m\\vec{a}$$$$Newton = \\frac{kg  m}{s^2}$$The unit of force is the Newton, mass is measured in kilograms ($kg$), and acceleration is measured in meters per second squared ($\\frac{m}{s^2}$).
+            <strong>Concept:</strong>Newton's Second Law of Motion:$$\\sum{\\vec{F}} = m\\vec{a}$$The unit of force is the Newton, mass is measured in kilograms ($kg$), and acceleration is measured in meters per second squared ($\\frac{m}{s^2}$).
 
 
 
